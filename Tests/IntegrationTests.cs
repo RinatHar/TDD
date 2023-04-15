1c3b06b00f3b4c74fc5fe24532fb587a5a59a0d3
Ринат Харисов <rinat.kharisov.85@mail.ru>
2023-04-15 09:26:44 +0500

    Добавьте файлы проекта.

---
+++ Tests/IntegrationTests.cs
@@ -0,0 +1,53 @@
+using System.Drawing;
+using System.IO;
+
+namespace Tests
+{
+    public class IntegrationTests
+    {
+        private ImageEdit.ImageEditor editor;
+        private readonly string filePathSave = "results";
+        private string folderPath;
+
+        [SetUp]
+        public void Setup()
+        {
+            editor = ImageEdit.ImageEditor();
+            folderPath = Path.Combine(Environment.CurrentDirectory, filePathSave);
+
+            if (Directory.Exists(folderPath))
+            {
+                Directory.Delete(folderPath, true);
+            }
+        }
+
+        [TestCase("1.jpg", 1)] // загрузка одной картинки
+        [TestCase("test_images", 3)] // загрузка директории с картинками
+        public void TestLoad(string filePath, int count_images)
+        {
+
+            bool result = editor.Load(filePath);
+
+            Assert.That(result, Is.True);
+            Assert.That(editor.Images.Count, count_images);
+        }
+
+        [TestCase("*.png")]
+        [TestCase("*.jpg")]
+        [TestCase("*.jpeg")]
+        [TestCase("*.bmp")]
+        [TestCase("*.gif")]
+        [TestCase("*.exif")]
+        [TestCase("*.tiff")]
+        public void TestSave(string format)
+        {
+            editor.Images.Add(new Bitmap(1000, 1000), new Bitmap(200, 300), new Bitmap(650, 150));
+
+            bool result = editor.Save(filePathSave, format);
+            string[] files = Directory.GetFiles(folderPath, format); // получаем все файлы из каталога с результатами нужного формата
+
+            Assert.That(result, Is.True);
+            Assert.That(files, Has.Length.EqualTo(3));
+        }
+    }
+}
---
+++ Tests/UnitTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class UnitTests
+    {
+        private ImageEdit.ImageEditor editor;
+
+        [SetUp]
+        public void Setup()
+        {
+            editor = ImageEdit.ImageEditor();
+        }
+
+        [TestCase(true, 100, 100, 100)] // изменение по ширине
+        [TestCase(false, 100, 100, 100)] // изменение по высоте
+        public void TestResizeImageWithSaveProp(bool dir, int size, int res_w, int res_h)
+        {
+            editor.Images.Add(new Bitmap(1000, 1000));
+
+            editor.Resize(dir, size);
+
+            foreach (Bitmap image in editor.Images)
+            {
+                Assert.That(image.Width, Is.EqualTo(res_w));
+                Assert.That(image.Height, Is.EqualTo(res_h));
+            }
+        }
+
+        [TestCase(100, 100, 100, 100)] // пропорции сохраняются
+        [TestCase(50, 100, 50, 100)] // пропорции НЕ сохраняются
+        public void TestResizeImageWithoutSaveProp(int w, int h, int res_w, int res_h)
+        {
+            editor.Images.Add(new Bitmap(1000, 1000));
+
+            editor.Resize(w, h);
+
+            foreach (Bitmap image in editor.Images)
+            {
+                Assert.That(image.Width, Is.EqualTo(res_w));
+                Assert.That(image.Height, Is.EqualTo(res_h));
+            }
+        }
+    }
+}
